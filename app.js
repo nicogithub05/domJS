dd814e522aa2819693060e70430dae50ae2f0411
nicogithub05 <nicolasgithub@gmail.com>
2024-11-20 13:10:46 -0500

    Utilizando document.querySelector y getElementBy

---
+++ app.js
@@ -0,0 +1,10 @@
+document.getElementById("app-title") // solo puede haber un id (nombre único)
+
+document.querySelector("#app-title") // podemos usar id´s, clases, etiquetas
+
+document.querySelector("p"); // solo trae el primer elemento
+
+document.getElementsByClassName("menu-items"); // nos  muestra una colección con todos los elementos que tengan esa clase
+
+document.getElementsByTagName("p") // nos muestra la coleccion html de esos elementos 
+
